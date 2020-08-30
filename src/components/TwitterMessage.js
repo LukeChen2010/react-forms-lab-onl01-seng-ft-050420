f512006a24318d9bc76967af039a0f57dd14e21b
Luke Chen <luke.chen.2010@gmail.com>
2020-08-30 03:01:29 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -21,7 +21,7 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" onChange={this.handleChange} />
+        <input type="text" onChange={this.handleChange} value={this.state.message}/>
         {remainingChars}
       </div>
     );
